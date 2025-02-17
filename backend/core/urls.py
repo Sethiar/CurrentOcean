e6c6f840418ac888ba817009cf6ea5aa82673ef4
Lefetey Arnaud <alefetey123@gmail.com>
2025-02-17 23:17:04 +0100

    Mise à jour de djanbgo core views et urls.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ocean_currents  # On retire CurrentListAPIView
+from .views import ocean_currents , index
 
 urlpatterns = [
     path('currents/', ocean_currents, name='currents_list'),
+    path("", index, name="index"),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+import os
+
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.http import HttpResponse
+
 from core.models import Current  # Import depuis core
+
+# Vue qui va permettre de servir la page home du site currentocean
+def index(request):
+    return redirect("http://localhost:5173/")
+
 
 def ocean_currents(request):
     currents = Current.objects.all()
     return render(request, 'core/current.html', {'currents': currents})
+
+

04bd827da239167c1a70fe44146350319692bfae
Lefetey Arnaud <alefetey123@gmail.com>
2025-02-17 23:14:27 +0100

    Mise à jour des fichiers sources.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Blog from "./pages/Blog";
-import ArticleDetail from "./pages/ArticleDetail.jsx";
+import ArticleDetail from "./pages/ArticleDetail";
+import Actualites from "./pages/Actualites";
+
+// Importation Header
+import Header from "./components/Header";
+// Importation Footer
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <Router>
+    <div>
+      {/* Header */}
+      <Header />
+
+      {/*</div> Routes de l'application */}
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:slug" element={<ArticleDetail />} />
+        <Route path="/actualites" element={<Actualites />} />
       </Routes>
-    </Router>
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Blog from "./pages/Blog";
-import Actualites from "./pages/Actualites";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/actualites" element={<Actualites />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );

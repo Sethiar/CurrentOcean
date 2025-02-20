b37dc3e8f48d6c465102565143446d098762f036
Lefetey Arnaud <alefetey123@gmail.com>
2025-02-20 21:16:09 +0100

    Sauvegarde des packages.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // Chemin correct
+    emptyOutDir: true, // Supprime l'ancien build avant de recréer
+  },
+
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:8000',
+    }
+  }
+});
